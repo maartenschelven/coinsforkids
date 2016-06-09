243b0f94208063d9d4704bf77fdabd2b3694a189
Maarten (M.J.J.) van Schelven <maarten.van.schelven@ing.nl>
2016-06-09 14:58:33 +0200

    added coins and movement

--- js/balk.js
+++ js/balk.js
@@ -12,8 +12,8 @@ function Balk() {
 
         //BoxGeometry(width, height, depth, widthSegments, heightSegments, depthSegments)
 
-        var x = 0;
-        var y = 2;
+        var x = -4;
+        var y = 0;
 
         var texture = new THREE.TextureLoader().load("images/wood.jpg");
         texture.wrapS = THREE.RepeatWrapping;
@@ -23,35 +23,29 @@ function Balk() {
 
         var material = new THREE.MeshBasicMaterial( {map: texture, transparent: true, opacity: 0.9} );
 
-        var frontGeometry = new THREE.BoxGeometry(12, 1, 0.25 );
+        var frontGeometry = new THREE.BoxGeometry(16, 1, 0.25 );
         var frontBalk = new THREE.Mesh( frontGeometry, material );
         frontBalk.position.x = x;
         frontBalk.position.y = y + 1;
 
-        frontBalk.rotation.x = 0.2;
-        frontBalk.rotation.y = -0.2;
         frontBalk.rotation.z = -0.2;
 
         var materialNotTransparent = new THREE.MeshBasicMaterial( {map: texture} );
 
-        var backGeometry = new THREE.BoxGeometry(12, 1.8, 0.25 );
+        var backGeometry = new THREE.BoxGeometry(16, 1.8, 0.25 );
         var backBalk = new THREE.Mesh( backGeometry, materialNotTransparent );
         backBalk.position.x = x + 0.27;
         backBalk.position.y = y + 1.6;
         backBalk.position.z = -1;
 
-        backBalk.rotation.x = 0.2;
-        backBalk.rotation.y = -0.2;
         backBalk.rotation.z = -0.2;
 
-        var bottomGeometry = new THREE.BoxGeometry(12, 0.5, 1.5 );
+        var bottomGeometry = new THREE.BoxGeometry(16, 0.5, 1.5 );
         var bottomBalk = new THREE.Mesh( bottomGeometry, materialNotTransparent );
         bottomBalk.position.x = x;
         bottomBalk.position.y = y + 0.8;
         bottomBalk.position.z = -0.5;
 
-        bottomBalk.rotation.x = 0.2;
-        bottomBalk.rotation.y = -0.2;
         bottomBalk.rotation.z = -0.2;
 
         scene.add(frontBalk);
--- js/coin.js
+++ js/coin.js
@@ -3,11 +3,17 @@
  */
 
 
-function Coin(name, value) {
+function Coin(placeInRow, name, type, value, position) {
     this.coinCylinder;
+    this.placeInRow = placeInRow
     this.name = name;
+    this.type = type
     this.value = value;
-    this.bounce = 0;
+    this.position = position;
+    this.startPostion = 0;
+    this.numberOfBounces = 0;
+    this.correctionHeight = position.x * -0.285;
+
 
     this.getCoinCylinder = function() {
         if (this.coinCylinder == null) {
@@ -17,7 +23,7 @@ function Coin(name, value) {
     }
 
     this.imagePath = function() {
-        return "images/" + this.name + " cent.png";
+        return "images/" + this.name + " " +this.type + ".png";
     }
 
     this.createCoinCylinder = function() {
@@ -25,20 +31,73 @@ function Coin(name, value) {
         coinTexture.wrapS = THREE.RepeatWrapping;
         coinTexture.wrapT = THREE.RepeatWrapping;
         coinTexture.repeat.set( 1, 1 );
-        var coinOneGeometry = new THREE.CylinderGeometry(1,1,0.2,32,1,false);
+        var coinOneGeometry = new THREE.CylinderGeometry(0.6,0.6,0.1,32,1,false);
         var coinOneMaterial = new THREE.MeshBasicMaterial( {map: coinTexture} );
         this.coinCylinder = new THREE.Mesh( coinOneGeometry, coinOneMaterial );
-        this.coinCylinder.position.z = 0; // Depth
-        this.coinCylinder.position.y = 0; // Vertical
-        this.coinCylinder.position.x = 0; // Horizontal
-        this.coinCylinder.rotation.x = 1.5;
+
+        this.coinCylinder.position.x = this.position.x // Horizontal
+        this.coinCylinder.position.y = this.position.y // Vertical
+        this.coinCylinder.position.z = -0.4; // Depth
+
+        this.coinCylinder.rotation.x = 1.6;
         this.coinCylinder.rotation.y = 1.6;
+
     }
 
-    this.startDropAnimation = function() {
-        if (this.bounce < 10) {
-            this.bounce += 0.1;
-            this.coinCylinder.position.y = Math.abs(Math.sin(this.bounce)) / this.bounce;
+    this.startDropSelectAnimation = function() {
+
+        var done = false;
+        if (this.numberOfBounces == 3) {
+            done = true;
+        } else {
+            // if y is 0 three times (three times bounce is done
+            if (this.numberOfBounces <= 3) {
+                this.startPostion += 0.1;
+                this.coinCylinder.position.y = ((Math.abs(Math.sin(this.startPostion * 0.5)) / this.startPostion) * 6.15) + this.correctionHeight;
+                if (this.coinCylinder.position.y < this.correctionHeight + 0.01) {
+                    this.numberOfBounces++
+                }
+            }
+
+            if (this.numberOfBounces == 1) {
+                this.coinCylinder.rotation.y = this.coinCylinder.rotation.y - 0.1;
+                this.coinCylinder.position.x = this.coinCylinder.position.x + 0.01;
+                this.correctionHeight = this.correctionHeight - 0.00019;
+            }
+            if (this.numberOfBounces > 1) {
+                this.coinCylinder.rotation.y = this.coinCylinder.rotation.y - 0.1;
+                this.coinCylinder.position.x = this.coinCylinder.position.x + 0.03;
+                this.correctionHeight = this.correctionHeight - 0.006;
+
+            }
+        }
+
+        return done;
+    }
+
+    this.rollThroughBalk = function() {
+
+        if (this.coinCylinder.position.x < 7) {
+            this.coinCylinder.rotation.y = this.coinCylinder.rotation.y - 0.4;
+            this.coinCylinder.position.x = this.coinCylinder.position.x + 0.1;
+            this.coinCylinder.position.y = this.coinCylinder.position.y - 0.020;
+            return false;
+        } else {
+            return true;
         }
     }
+
+    this.startDropToPiggyBank = function () {
+        if (this.coinCylinder.position.y > -7) {
+            this.coinCylinder.rotation.y = this.coinCylinder.rotation.y - 0.2;
+            this.coinCylinder.rotation.x = this.coinCylinder.rotation.x - 0.04;
+            this.coinCylinder.rotation.z = this.coinCylinder.rotation.z - 0.02;
+
+            this.coinCylinder.position.y = this.coinCylinder.position.y - 0.05;
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
